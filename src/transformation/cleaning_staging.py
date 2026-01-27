930c94806b6bf908642ef48b436eeedb8bcea956
FlavLbr <79084721+FlavLbr@users.noreply.github.com>
2026-01-27 15:39:45 +0000

    staging table session, session_result et starting_grid

--- src/transformation/cleaning_staging.py
+++ src/transformation/cleaning_staging.py
@@ -45,6 +45,7 @@ meetings = meet.loc[meet["meeting_name"].str.contains("Grand Prix"),["meeting_ke
 verification_dossier_parents("meetings", niveau)
 enregistrement_donnees(niveau,meetings,"meetings")
 
+
 # Création de la table race_control
 race_control=lecture_fichier_json("race_control")
 race_control=race_control.loc[((race_control["category"].isin(["Flag","SafetyCar"]))&(~race_control["flag"].isin(["BLUE","BLACK AND WHITE"])))|((race_control["category"]=="Other") & (race_control["message"].str.contains("SECOND TIME PENALTY")) & ~race_control["message"].str.contains("PENALTY SERVED")),["session_key","lap_number","category","flag","scope","sector","qualifying_phase","message"]].drop_duplicates()
@@ -54,3 +55,27 @@ race_control["sector"] = race_control["sector"].astype('Int64')
 race_control["qualifying_phase"] = race_control["qualifying_phase"].astype('Int64')
 verification_dossier_parents("race_control", niveau)
 enregistrement_donnees(niveau,race_control,"race_control")
+
+
+# Création de la table session_result
+session_result=lecture_fichier_json("session_result")
+session_result = session_result[["position","driver_number","number_of_laps","dnf","dns","dsq","duration","gap_to_leader","session_key","points"]]
+session_result["number_of_laps"] = session_result["number_of_laps"].astype('Int64')
+session_result["duration"] = session_result["duration"].astype(str)
+session_result["gap_to_leader"] = session_result["gap_to_leader"].astype(str)
+verification_dossier_parents("session_result", niveau)
+enregistrement_donnees(niveau,session_result,"session_result")
+
+
+# création des tables sessions
+sessions=lecture_fichier_json("sessions",True)
+sessions = sessions[["session_key","session_type","session_name","date_start","date_end","meeting_key","year"]]
+verification_dossier_parents("sessions", niveau)
+enregistrement_donnees(niveau,sessions,"sessions")
+
+
+# Création de la table starting_grid
+starting_grid=lecture_fichier_json("starting_grid")
+starting_grid = starting_grid[["position","driver_number","lap_duration","session_key"]]
+verification_dossier_parents("starting_grid", niveau)
+enregistrement_donnees(niveau,starting_grid,"starting_grid")
